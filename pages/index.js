973ec41334a580d3d8d52d03bbbd1634d2bf8f8e
wulianglongrd <wulianglongrd@gmail.com>
2018-09-13 21:17:14 +0800

    support less

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-export default () => 'react ssr app !'
+import styles from './index.less'
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div className={styles.red}>
+        react ssr app !
+      </div>
+    )
+  }
+}
